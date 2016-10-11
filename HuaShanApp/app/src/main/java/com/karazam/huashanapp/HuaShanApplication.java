5e1af615d7e1e19d5aeee2081c65610ceb569ba5
awplying12 <awplying14@163.com>
2016-10-11 15:14:06 +0800

    tongbu

---
+++ HuaShanApp/app/src/main/java/com/karazam/huashanapp/HuaShanApplication.java
@@ -0,0 +1,15 @@
+package com.karazam.huashanapp;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2016/10/11.
+ */
+
+public class HuaShanApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
